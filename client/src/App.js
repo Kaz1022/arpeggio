c6ba68377973745e211b1c177e2172f4b88733c2
Michaela-K <michaelakodes@gmail.com>
2022-07-28 13:49:36 -0400

    add bootstrap navbar to Nav1 component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.scss';
-import Nav from './components/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav1 from './components/Nav1';
 
 function App() {
  return (
   <div className="App">
-   <Nav></Nav>
+   <Nav1/>
   </div>
  );
 }

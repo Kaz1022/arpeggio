a4bcf6b40a9a56e985f13c038ddda10382f954b4
Michaela-K <michaelakodes@gmail.com>
2022-07-29 10:04:49 -0400

    create custome file for colour, trial font display

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import './scss/custom.scss';
+// import './scss/_fonts.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

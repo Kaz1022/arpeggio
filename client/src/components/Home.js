dd15785d60dcbc868a515ad084c49bac27183846
Michaela-K <michaelakodes@gmail.com>
2022-07-30 11:51:04 -0400

    add user images and position

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import '../scss/custom.scss';
+//ICONS
 import main from '../assets/images/main.jpeg';
 import connect from '../assets/images/connect.jpg';
 import collaborate from '../assets/images/collaborate.jpg';
 import learn from '../assets/images/learn.jpg';
 import create from '../assets/images/create.jpg';
+//USER IMAGES
+import marina from '../assets/images/marina.jpg';
+import kaz from '../assets/images/kaz.jpg';
+import michaela from '../assets/images/michaela.jpg';
 
 
 const Img = styled.img.attrs({
@@ -95,25 +100,27 @@ const Container = styled.div`
  .create {
   height: auto;
   width: 21rem;
-  font-size: 1.5rem;
  }
  .user1,
  .user2,
  .user3 {
-  height: 10rem;
-  width: 35rem;
+  height: auto;
+  width: 30rem;
+  padding: 3rem;
+  text-align:center;
  }
 
  .feature {
   font-family: 'Oswald', sans-serif;
   margin: 15px 0px 15px 0px;
+  font-size:1.5rem;
  }
  .description {
   font-family: 'Roboto', sans-serif;
   font-size: 1.3rem;
  }
 `;
-
+//FEATURE ICONS
 const ConnectIcon = styled.img.attrs({
  src: `${connect}`,
 })`
@@ -143,6 +150,34 @@ const CollaborateIcon = styled.img.attrs({
 
  const CreateIcon = styled.img.attrs({
   src: `${create}`,
+ })`
+  width: auto;
+  height: 100%;
+  object-fit: cover;
+ }
+ `;
+
+ //USER ICONS
+ const MarinaIcon = styled.img.attrs({
+  src: `${marina}`,
+ })`
+  width: auto;
+  height: 100%;
+  object-fit: cover;
+ }
+ `;
+
+ const KazIcon = styled.img.attrs({
+  src: `${kaz}`,
+ })`
+  width: auto;
+  height: 100%;
+  object-fit: cover;
+ }
+ `;
+
+ const MichaelaIcon = styled.img.attrs({
+  src: `${michaela}`,
  })`
   width: auto;
   height: 100%;
@@ -182,24 +217,24 @@ function Home() {
    </section>
    <section className="artists">
     <div className="user1">
-     <div className="icon">USER ICON</div>
+    <MarinaIcon src="../assets/images/marina.jpg" />
      <div className="feature">MARINA BEKKER</div>
      <div className="description">
-      Before Arpeggio, It was so difficult to find someone to .......Lorem Ipsum
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis finibus ex, sit amet faucibus ante. Integer vel est sit amet mi pharetra maximus. Integer et augue sagittis, semper dui et, gravida elit. Quisque pellentesque commodo mi, vitae aliquam ante euismod quis. Praesent lobortis dictum urna faucibus ultrices. Vivamus tincidunt mi eget nulla laoreet, a consectetur sapien mollis.
      </div>
     </div>
     <div className="user2">
-     <div className="icon">USER ICON</div>
-     <div className="feature">MARINA BEKKER</div>
+    <KazIcon src="../assets/images/kaz.jpg" />
+     <div className="feature">KAZ BAYNTON</div>
      <div className="description">
-      Before Arpeggio, It was so difficult to find someone to .......Lorem Ipsum
+     Aenean tempor dui pretium lorem tempor, vitae commodo nulla elementum. Ut semper orci rhoncus odio pellentesque, eu viverra leo tempus. Mauris tempus, mi ac molestie posuere, sem nisl laoreet libero, sit amet viverra orci ipsum at quam. Phasellus quam orci, dignissim sit amet diam eu, rhoncus ornare nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
      </div>
     </div>
     <div className="user3">
-     <div className="icon">USER ICON</div>
-     <div className="feature">MARINA BEKKER</div>
-     <div className="description">
-      Before Arpeggio, It was so difficult to find someone to .......Lorem Ipsum
+    <MichaelaIcon src="../assets/images/michaela.jpg" />
+      <div className="feature">MICHAELA KING</div>
+      <div className="description">
+      Morbi dapibus nec libero vitae accumsan. Nunc condimentum mattis quam eu tincidunt. Morbi pellentesque dictum mauris in ultrices. Pellentesque dignissim leo in vestibulum convallis. Vestibulum sollicitudin dolor sit amet lobortis tempus. Maecenas quis pulvinar metus, quis viverra dolor. Ut maximus, sem non dictum vulputate, tortor arcu tempor nulla. Nec consectetur libero vitae accumsantst.
      </div>
     </div>
    </section>

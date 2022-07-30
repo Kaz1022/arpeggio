9c5bbbb5d5c515d707ed59139c3df6891b945b9e
Michaela-K <michaelakodes@gmail.com>
2022-07-30 14:13:52 -0400

    finalise footer and add react-icons to package-json

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,18 +5,25 @@ import '../scss/custom.scss';
 const Styles = styled.div`
  height: 10vh;
  width: 100%;
- background-color: #BB0DBE;
+ background-color: #bb0dbe;
+ display:flex;
+ align-items: center;
 
-
+ .footer-container {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 58px 20px 58px;
+  color: white;
+ }
 `;
 
 function Footer() {
  return (
   <Styles>
-   <div>ICONS
-    <div>icon1</div>
-    <div>icon2</div>
-    <div>icon3</div>
+   <div className="footer-container">
+    <div className="x">&copy; 2022 KBB Productions</div>
    </div>
   </Styles>
  );

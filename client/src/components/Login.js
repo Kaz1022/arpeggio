bc14fb5ea8b629d45895669b4539f9b3f418f5bc
Marina Bekker <mshooch@gmail.com>
2022-07-31 23:43:54 -0400

    add route to login

---
+++ client/src/components/Login.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import '../scss/signup.scss';
+
+class Login extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			handle: '',
+			password: '',
+
+		}
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+  handleHandleChange = (e) => {
+		this.setState({
+			handle: e.target.value,
+		});
+	}
+
+  handlePasswordChange = (e) => {
+		this.setState({
+			password: e.target.value,
+		});
+	}
+
+
+	handleSubmit = (e) => {
+    console.log("submitted")
+
+	}
+
+
+render() {
+  return (
+		<div className="base-container">
+		<div className="header"><h1>Log in</h1></div>
+		<div className="content">
+			<div className="form">
+    		<div className="form-group">
+	  			<form onSubmit={this.handleSubmit}>
+		  
+				<input
+          onChange={this.handleHandleChange}
+          type="text"
+          name="handle"
+          placeholder="Username"
+          required
+          />
+
+					<input
+          onChange={this.handlePasswordChange}
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+          />
+
+        <button type="submit">
+          Login
+        </button>
+    </form>
+		</div>
+		</div>
+		</div>
+		</div>
+		);
+	}
+}
+export default Login;

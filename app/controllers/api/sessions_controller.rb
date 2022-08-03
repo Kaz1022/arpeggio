67ed9703f2805506c1846b4b45fa65152a81e9b0
Kazuko Baynton <kazukobaynton@gmail.com>
2022-08-03 09:54:03 -0700

    add logged_in? logout logic

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,39 +1,50 @@
 class Api::SessionsController < ApplicationController
 #   skip_before_action :authenticate_user, only: [:create]
-  
-  def new
-    # not used since have front end login page
-end
+    
+    include CurrentUserConcern
 
-def create
-    user = User.find_by_email(user_params[:email])
-    # If the user exists AND the password entered is correct.
-    if user && user.authenticate(user_params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
-      # logged in when they navigate around our website.
-      session[:user_id] = user.id
+    def create
+        user = User.find_by_email(user_params[:email])
+        # If the user exists AND the password entered is correct.
+        if user && user.authenticate(user_params[:password])
+        # Save the user id inside the browser cookie. This is how we keep the user 
+        # logged in when they navigate around our website.
+        session[:user_id] = user.id
 
-      # rails cookie that can be accessed on every controller by:   cookies[:user]
-      user ||= cookies[:user].present? ? JSON.parse(cookies[:user]) : {}
+        # rails cookie that can be accessed on every controller by:   cookies[:user]
+        user ||= cookies[:user].present? ? JSON.parse(cookies[:user]) : {}
 
-      # render json: session[:user_id]
-      render json: {
-          status: :created,
-          logged_in: true,  
-          user: user
-        }
-    else
-      render json: { status: 401, errors: ['Please try again!'] }
+        # render json: session[:user_id]
+        render json: {
+            status: :created,
+            logged_in: true,  
+            user: user
+            }
+        else
+        render json: { status: 401, errors: ['Please try again!'] }
+        end
     end
-end
 
-def destroy
-    session[:user_id] = nil
-    redirect_to '/login'
-end
+    def logged_in
+        if @current_user
+            render json: {
+                logged_in: true;
+                user: @current_user
+            }
+        else
+            render json: {
+                logged_in: false
+            }
+        end
+    end
 
-private
-def user_params
-  params.require(:user).permit(:email, :password)
-end
+    def logout
+        reset_session
+        render json : { status: 200, logged_out: true }
+    end
+
+    private
+    def user_params
+    params.require(:user).permit(:email, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
     resources :events
     resources :sessions, only: [:create]
 
+    get :logged_in, to: 'sessions#logged_in'
+    delete :logout, to: 'sessions#logout'
     post :signup, to: "users#create"
-    delete :logout, to: 'sessions#destroy'
   end
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }

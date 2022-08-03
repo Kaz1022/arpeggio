46e0c01591233f8648c027107ebc0a64b822999d
Michaela-K <michaelakodes@gmail.com>
2022-08-02 21:24:33 -0400

    add eventfilter.js WIP

--- client/src/components/Events/EventList.js
+++ client/src/components/Events/EventList.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import EventListItem from './EventListItem';
+import EventFilter from './EventFilter';
 import styled from 'styled-components';
 import '../../scss/custom.scss';
 
 
-const events_api_url = `http://localhost:3000/api/events`;
+const api_url = `http://localhost:3000/api/events`;
 
 const Styles = styled.div`
 .event{
@@ -16,7 +17,7 @@ function EventList() {
  const [state, setState] = useState({ items: [] });
 
  function getEvents() {
-  fetch(events_api_url)
+  fetch(api_url)
    .then((response) => response.json())
    .then((response_items) => {
     setState({ items: response_items });
@@ -30,12 +31,13 @@ function EventList() {
  console.log(state.items);
  return (
   <Styles>
-   <div className="event">
+    <div className="event">
     {state.items.map((item) => {
      return (
-      // <>
+      <>
+       <EventFilter prompt="Select"/>
        <EventListItem key={item.id} title={item.title} date={item.event_date} start={item.start_time} end={item.end_time} city={item.city} country={item.country} level={item.level} venue={item.venue_style} genre={item.genre} image={item.event_image} description={item.description} status={item.post_active} created={item.created_at}/>
-      // </>
+      </>
      );
     })}
    </div>

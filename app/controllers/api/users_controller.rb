d70c67a98605c5d8df889da92b3dfc444c056daa
Marina Bekker <mshooch@gmail.com>
2022-08-04 00:57:23 -0400

    creat skeleton new event form

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -46,40 +46,6 @@ class Api::UsersController < ApplicationController
     
   end
   
-  # alternative ways for the params:
-  # def create
-  #   user = User.new(user_params)
-  #   if user
-  #     session[:user_id] = user.id
-  #     render json: user, status: :created
-  # else
-  #     render json: user.errors.full_messages, status: :unprocessable_entity
-  #     render json: {
-  #       status:created,
-  #       user:user
-  #     }
-  #   else
-  #     render json: {status: 500 }
-  #   end
-  # end
-
-  # private
-
-  # def user_params
-  #   params.require(:user).permit(
-  #     :first_name,
-  #     :last_name,
-  #     :handle,
-  #     :email,
-  #     :country,
-  #     :password,
-  #     :password_confirmation,
-  #     :profile_image,
-  #     :city,
-  #     :phone
-  #   )
-  # end
-
 
 # class Api::UsersController < ApplicationController
 #   skip_before_action :authenticate_user

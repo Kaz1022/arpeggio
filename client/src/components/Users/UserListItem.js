6c7f409babd50a311fbfd94046333b306a2f68fc
Kazuko Baynton <kazukobaynton@gmail.com>
2022-08-06 13:52:11 -0700

    add bio table

--- client/src/components/Users/UserListItem.js
+++ client/src/components/Users/UserListItem.js
@@ -62,7 +62,7 @@ const UserStyles = styled.div`
   left: 92%;
   bottom: 5%;
 }
-.event-details{
+.user-details{
   display: flex;
   flex-direction:column;
   flex-wrap:wrap;
@@ -72,26 +72,16 @@ const UserStyles = styled.div`
   justify-content:space-between;
   padding-right: 10rem;
 }
-.spots-heading{
-  font-family: 'Oswald', sans-serif;
-  font-size: 1.4rem;
-}
-.event-description{
-  font-size: 1.2rem;
-  margin: 30px 0px;
-  line-height: 30px;
-}
-.instrument-icons{
-  width: 50%;
-  display:flex;
-  justify content: space-around;
-}
 
 //RIGHT
 .right{
   height: 100%;
   // width:100%;
   flex:1;
+}
+
+.bio{
+  font-size: 1rem;
 }
 `;
 
@@ -115,17 +105,16 @@ function UserListItem(props) {
         <div className="user-details">
           <div className="details">
             <div className="details1">
-              <div className="event-location"><strong>Location:&nbsp;&nbsp;</strong>{props.currentUser.city}, {props.currentUser.country}</div>
-              <div className="event-level"><strong>Instrument(s) I play:&nbsp;&nbsp;</strong> {userInstruments}.</div>
+              <div className="user-location"><strong>Location:&nbsp;&nbsp;</strong>{props.currentUser.city}, {props.currentUser.country}</div>
+              <div className="user-instruments"><strong>Instrument(s) I play:&nbsp;&nbsp;</strong> {userInstruments}.</div>
             </div>
             <div className="details2">
                 <div className="about-me"><strong>About Me...:&nbsp;&nbsp;</strong></div>
-                <div className="bio">BIO BLAH BLAH </div>
+                <div className="bio">{props.currentUser.bio}</div>
             </div>
           </div>
         </div>
       </div>
-      {console.log('image .>>', props.currentUserImage)}
 
       <div className="right">
       {props.currentUserImage.length > 0 && <Img src={`http://localhost:3000/${props.currentUserImage}`} /> }

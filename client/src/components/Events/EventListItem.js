27233926ababef92696168c29c3feb6522165beb
Michaela-K <michaelakodes@gmail.com>
2022-08-09 06:58:10 -0400

    show image on main events page

--- client/src/components/Events/EventListItem.js
+++ client/src/components/Events/EventListItem.js
@@ -5,7 +5,6 @@ import TimeAgo from 'react-timeago';
 import '../../scss/custom.scss';
 import '../../App.scss';
 
-import main from '../../assets/images/main.jpeg';
 import drumsA from '../../assets/images/music_icons/Drums/drums_a.png';
 import guitarA from '../../assets/images/music_icons/Guitars/guitar_a.png';
 import vocalA from '../../assets/images/music_icons/Vocals/vocals_a.png';
@@ -56,17 +55,6 @@ const GuitarImg = styled.img.attrs({
     outline: none;
      }}`;
 
-const Img = styled.img.attrs({
- src: `${main}`,
-})`
-  max-width: 100%;
-  height: 100%;
-  flex-shrink: 0;
-  object-fit: cover;
-  // opacity: 0.92;
- }
- `;
-
 const EventStyles = styled.div`
 .card{
   display:flex;
@@ -76,7 +64,7 @@ const EventStyles = styled.div`
   padding: 0rem 5rem;
 }
 .eventCard{
-  height: fit-content
+  height: fit-content;
   // max-height: 90vh;
   width: 100%;
   margin: 30px;
@@ -137,7 +125,7 @@ const EventStyles = styled.div`
   line-height: 30px;
 }
 .instrument-icons{
-  width: 50%;
+  width: 75%;
   display:flex;
   justify content: space-around;
 }
@@ -145,8 +133,8 @@ const EventStyles = styled.div`
 //RIGHT
 .right{
   position: relative;
-  height: auto;
-  width:auto;
+  height: 100%;
+  width:100%;
   padding: 0;
   margin: 0;
   flex:1;
@@ -157,6 +145,12 @@ const EventStyles = styled.div`
     position: absolute;
     top:94%;
     left: 40%;
+  }
+  .main_image{
+    max-width: 100%;
+    height: 55vh;
+    // flex-shrink: 0;
+    object-fit: cover;
   }
 }
 `;
@@ -299,14 +293,13 @@ const handleConfirm = () => setStatuss();
        <div className="spots-heading">AVAILABLE SPOTS</div>
        <ConfirmationModal show={show} onHide={handleClose} onConfirm={handleConfirm}/>
        <div className="instrument-icons">
-        <div onClick={handleShow}>{getEventData()}</div>
+        <div className="icons" onClick={handleShow}>{getEventData()}</div>
        </div>
       </div>
      </div>
 
      <div className="right">
-      <img src={image} alt="" />
-      <Img src="../assets/images/main.jpeg" />
+      <img className="main_image" src={image} alt="" />
       <TimeAgo className="timeago" date={created}></TimeAgo>
      </div>
     </div>

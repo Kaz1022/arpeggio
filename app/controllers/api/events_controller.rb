5b8a50c404c095ebe202a498949d266b7ec5eacb
Kazuko Baynton <kazukobaynton@gmail.com>
2022-08-12 09:30:28 -0700

    delete comments

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -4,13 +4,6 @@ class Api::EventsController < ApplicationController
   # GET /events
   def index
     @events = Event.with_attached_event_image.includes([:user, :event_instruments, :instruments]).all
-    # 100 + 1 query problem
-
-   # Event1 - EventInstruaments for event 1 - instrument 
-   # Event2 - event_instruments for event 2
-   # Event3 - 
-   # Event4 - 
-
     render json: @events.as_json(:include => [:user, :event_instruments, :instruments], methods: [:event_image_data])
   end
 
@@ -52,28 +45,6 @@ class Api::EventsController < ApplicationController
   def new
     @event = Event.new
   end
-
-
-  # # POST /events
-  # def create
-
-  #   # instrument = EventInstrument.new(instrument_params)
-  #   # instrument.save!
-  #   # params[:instrument_id] = instrument[:instrument_id]
-
-  #   @event = Event.new(event_params)
-
-  #   if @event.save
-  #     render json: {
-  #       status: :created, 
-  #       event: @event
-  #       # location: @event
-  #   }
-  #   else
-  #     render json: @event.errors
-  #     # render :new
-  #   end
-  # end
 
   def create1
     ActiveRecord::Base.transaction do
